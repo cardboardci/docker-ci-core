5e8dc151d5f38d258b6fddce231f6c80618bf85c
jrbeverly <jrbeverly@live.com>
2019-11-02 18:24:34 -0400

    Try setting the github remote

--- .github/actions/dependencies/entrypoint.sh
+++ .github/actions/dependencies/entrypoint.sh
@@ -12,8 +12,11 @@ apt-get update
 mv provision/pkglist provision/pkglist.bak
 touch provision/pkglist
 while read p; do
+    echo "Working with ${p}"
     input=(${p//=/ })
+    echo "Determining version for ${input}"
     version=$(apt-cache show ${input} | grep 'Version: ' | awk -F"[ ',]+" '/Version:/{print $2}')
+    echo "Found version as ${version}"
     echo "${input}=${version}" >> provision/pkglist
 done <provision/pkglist.bak
 
@@ -22,4 +25,5 @@ cat provision/pkglist
 git checkout -b gh-actions/update-dependencies
 git add provision/pkglist
 git commit -m 'Updating the dependencies' --allow-empty
-git push -u origin HEAD
+git remote add github "https://${GITHUB_ACTOR}:${GITHUB_TOKEN}@github.com/${GITHUB_REPOSITORY}.git"
+git push -u github HEAD

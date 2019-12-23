179d4347141a3cb00d933425dbfbe7f8d48fcd43
jrbeverly <jrbeverly@live.com>
2019-12-22 19:33:43 -0500

    Remove dependency on sudo

--- provision/user.sh
+++ provision/user.sh
@@ -2,8 +2,7 @@
 set -e
 
 groupadd -r cardboardci 
-useradd --password "" --no-log-init -r -g cardboardci cardboardci
-adduser cardboardci sudo
+useradd --no-log-init -r -g cardboardci cardboardci
 
 mkdir -p /home/cardboardci
 chown cardboardci /home/cardboardci
